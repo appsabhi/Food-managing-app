1918874cdadd84021d66262d757eed9b7909222d
Abhinand P <abhinandappuabhinand@gmail.com>
2024-10-26 11:49:18 +0530

    updated some improvement on frontend

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -71,6 +71,7 @@ const Homepage = () => {
         favcopy.push(fav);
         setfavourites(favcopy);
         localStorage.setItem("favourites", JSON.stringify(favcopy));
+        window.scrollTo({top:"0",behavior:"smooth"})
       } else {
         alert("already exist in the favourites");
       }
@@ -125,17 +126,22 @@ return item.title.toLowerCase().includes(filtereditem.filtervalue);
   console.log("apistatus",succesapi)
 
   return (
-    <>
+    <div className="w-full h-full flex justify-center items-center">
+    
+    {loadingstate ? (
+           <div className="w-20 h-20 flex self-center  absolute z-20 border-8 rounded-full border-r-blue-600 animate-spin   ">
+         
+          </div>
+        ) : null}
       {/* loading state */}
       <div
       
         className={
           theme
-            ? "w-full h-full p-10 bg-yellow-600 flex flex-col items-center gap-10 overflow-x-scroll"
-            : "w-full h-full p-10 bg-emerald-950 flex flex-col items-center gap-10 overflow-x-scroll"
+            ? "w-full h-full p-10 bg-yellow-600 flex flex-col items-center gap-10 overflow-x-scroll relative z-10"
+            : "w-full h-full p-10 bg-emerald-950 flex flex-col items-center gap-10 overflow-y-scroll  overflow-x-hidden  relative z-10"
         }
       >
-
 
 
         <Changetheme />
@@ -169,14 +175,16 @@ return item.title.toLowerCase().includes(filtereditem.filtervalue);
         </div>
       
         {/* favourites */}
-        <h1 className="text-center  text-3xl text-orange-600  ">
-          Favourites
-        </h1>
+       
 
       
      
-       <div className="w-full h-full   flex  flex-nowrap  items-center gap-5 justify-center ">
-          {filteredfavourites && filteredfavourites.length
+       <div className="w-full h-full   flex flex-col  items-center gap-5 justify-center  ">
+       <h1 className="text-center  text-3xl text-orange-600  ">
+          Favourites
+        </h1>
+        <div className={filteredfavourites.length ?"w-full h-full    flex gap-10  overflow-x-scroll " :"w-full h-full    flex gap-10   "}>
+        {filteredfavourites && filteredfavourites.length
             ? filteredfavourites.map((fav) => (
                 <Favourites
                   favorite_item={fav}
@@ -184,26 +192,22 @@ return item.title.toLowerCase().includes(filtereditem.filtervalue);
                   key={fav.id}
                 />
               ))
-            : null}
+            :    <div className="w-full text-center text-5xl text-slate-600 ">No Favorites are Found</div>
+}
         </div>
-
-      
-         
-      
-        {loadingstate ? (
-           <div className="w-20 h-full  border-8 rounded-full border-r-yellow-400 border-b-yellow-200  border-emerald-950  animate-spin   ">
-         
-          </div>
-        ) : null}
+        </div>
+        
 
         {/* recipies list */}
         <div className="w-full h-full flex items-centre justify-center  gap-5  flex-wrap  ">
        
           
           {render_method()}
+
+          {!loadingstate && !recipies.length && <div className="text-5xl text-slate-600 ">No Recipes are Found</div>}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
